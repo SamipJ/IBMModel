1c0477b87242f83c3537168142d992af1f40b3fd
Tanvi Aggarwal <tanaaggarwal@gmail.com>
2017-04-15 15:26:48 +0530

    commented code

--- jaccard.py
+++ jaccard.py
@@ -1,30 +1,30 @@
 import re
 def computeJaccard(doc1,doc2):
-	#opening files in read-only mode
-    f1 = open(doc1,'r')
+					
+    f1 = open(doc1,'r')								#opening files in read-only mode
     f2 = open(doc2,'r')
-	#creating lists to store unique words in a document
-    d1 = []
+	
+    d1 = []									#creating lists to store unique words in a document
     d2 = []
-	#list containing the words common in both the documents
-    intersect = []
-	#parsing through each word in each line in doc1
-    for line in f1:
+	
+    intersect = []								#list containing the words common in both the documents
+	
+    for line in f1:								#parsing through each word in each line in doc1
         for word in re.compile('\w+').findall(line):
-			#adding the word to d1, if it is not already present
-            if word not in d1:
+			
+            if word not in d1:							#adding the word to d1, if it is not already present
                 d1.append(word)
-	#parsing through each word in each line in doc2
-    for line in f2:
+	
+    for line in f2:								#parsing through each word in each line in doc2
         for word in re.compile('\w+').findall(line):
-			#adding the word to d2, if it is not already present
-            if word not in d2:
+			
+            if word not in d2:							#adding the word to d2, if it is not already present
                 d2.append(word)
-				#adding this word to intersect, if it is present in d1
-                if word in d1:
+				
+                if word in d1:							#adding this word to intersect, if it is present in d1
                     intersect.append(word)
-	#calculating score=(no. of elements in intersection(doc1,doc2))/(no. of words in union(doc1,doc2))
-    score = float(len(intersect))/float((len(d1)+len(d2)-len(intersect)))
+									
+    score = float(len(intersect))/float((len(d1)+len(d2)-len(intersect)))	#calculating score=(no. of words in intersection(doc1,doc2))/(no. of words in union(doc1,doc2))
     print score
 
 def main():
